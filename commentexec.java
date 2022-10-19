7046b82e92af88a6ef51b59b04af80fb737617e5
hatset <73290263+hatset@users.noreply.github.com>
2022-10-19 14:03:03 +0530

    Comment execute

---
+++ commentexec.java
@@ -0,0 +1,9 @@
+
+
+public class Testing { 
+    public static void main(String[] args) 
+     { 
+         // the line below this gives an output 
+         // \u000d System.out.println("comment executed"); 
+     } 
+} 
